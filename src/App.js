14b539c3652a47f57abe7f00adc54b1aa47d3e3e
kmthein <kmthein96.mm@gmail.com>
2022-08-26 07:16:42 +0630

    assignment 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
+import DishDetail from './components/DishDetailComponent';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+render() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText} from 'reactstrap';
+
+class DishDetail extends Component{
+    
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )} 
+        else 
+        return (
+            <div></div>
+        )
+    }
+
+    renderComments(comments) {
+        if (comments != null) {
+            const commentView = comments.map((c) => <li key={c.id}>{c.comment}<br/>{'--' + c.author} {', ' + new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(c.date)))}</li>)
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    <ul className='list-unstyled'>{commentView}</ul>
+                </div>
+            )
+        } 
+    }
+
+    render() {
+        return(
+            <div>
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-4'>{this.renderDish(this.props.selectedDish)}</div>
+                    <div className='col-12 col-md-5 m-4'>{this.renderComments(this.props.selectedDish ? this.props.selectedDish.comments : null)}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ 
+export default DishDetail;
